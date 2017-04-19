0453224dbbf56cfc8a698af0d40cd50c5f0bc43d
genma123 <jemmerling@juno.com>
2017-04-18 21:34:04 -0400

    Got add new task to work

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
 		init: true,
 		tasks: []
     };
+	
+	this.add = this.add.bind(this);
   }
   
   retrieve() {
@@ -22,18 +24,46 @@ class App extends Component {
 		  console.log("tasks: " + JSON.stringify(data));
 		  app.setState({ tasks: data });
 	  });
-	  ;
+  }
+  
+  add(event) {
+	  /* console.log("In add, this: " + this + ", target: " + event.target);
+	  if (event.target) {
+		  console.log("value: " + event.target.elements.title.value);
+	  } */
+	  var app = this;
+	  var task = { title: event.target.elements.title.value, isDone: false };
+	  var body = JSON.stringify(task);
+	  console.log("Posting: " + body);
+	  fetch(`api/task`, {
+ 		  headers: {
+			'Accept': 'application/json, text/plain, */*',
+			'Content-Type': 'application/json'
+ 		  },
+ 		  method: "POST",
+		  body: body
+	  }).then(function(res) {
+		  return res.json();
+	  }).then(function(data) {
+		  var tasks = app.state.tasks.concat([data]);
+		  app.setState({ tasks: tasks });
+	  });
+	  event.preventDefault();
   }
   
   shouldComponentUpdate(nextProps, nextState) {
     // You can access `this.props` and `this.state` here
     // This function should return a boolean, whether the component should re-render.
 	let init = this.state.init;
-	return init || !_(this.state).differenceWith(nextState, _.isEqual).isEmpty();
+	console.log("this.state.tasks: " + JSON.stringify(this.state.tasks, null, 2));
+	console.log("nextState.tasks: " + JSON.stringify(nextState.tasks, null, 2));
+	// let diff = _(this.state.tasks).differenceWith(nextState.tasks, _.isEqual);
+	// console.log("diff: " + JSON.stringify(diff, null, 2));
+	return init || !_.isEqual(this.state.tasks.sort(), nextState.tasks.sort());
   }
   
   componentWillMount() {
-    this.retrieve();
+	this.retrieve();
   }
   
   componentDidUpdate() {
@@ -41,11 +71,12 @@ class App extends Component {
   }
   
   render() {
+	  console.log("IN App.render()");
 	return (
 		<div className="container">
 			<h1>MyTaskList</h1>
 			<hr/>
-			<form className="well">
+			<form className="well" onSubmit={this.add}>
 			   <div className="form-group">
 				   <input type="text" name="title" className="form-control" placeholder="Add Task..."/>
 			   </div>
